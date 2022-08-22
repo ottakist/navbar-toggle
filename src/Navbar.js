fbf3a142cc45645fb37dea5f706d5c94ce37bfd7
ottakist <102596775+ottakist@users.noreply.github.com>
2022-08-22 23:29:17 +0300

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState} from 'react'
+import { FaBars} from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [show,setShow] = useState(false)
+  // console.table(social);
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button
+            className='nav-toggle'
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className={`links-container ${show && `show-container`}`}
+          style={{
+            height: `${show ? links.length * 2.5 : 0}rem`,
+          }}
+        >
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((social) => {
+            const { id, url, icon } = social;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
